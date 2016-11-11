14e3653199473505315458a06fc4f259a0a86a09
MegaMind1000 <yanghansong1996@live.com>
2016-11-11 11:22:18 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,102 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char operation[5] = {'#','+','-','*','/'};
 
-float cal(float x, float y, int op){
-    switch(op){
-        case 1: return x+y;
-        case 2: return x-y;
-        case 3: return x*y;
-        case 4: return x/y;
+    char data1[19][10] = {"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve",
+                            "thirteen","forteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+
+    char data2[8][7] = {"twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+
+translation(long M){
+    long k;
+    if(M==0){
+        printf("Zero\n");
+        return;
+    }
+    k = M/1000000000;
+
+    if(k != 0){
+        translation_A(k);
+        printf("billion ");
+    }
+    k = M % 1000000000;
+    if(k!=0){
+        translation_A(k);
     }
 }
 
-float model1(float i, float j, float k, float t, int op1, int op2, int op3){
-    float r1,r2,r3;
-    r1 = cal(i,j,op1);
-    r2 = cal(r1,k,op2);
-    r3 = cal(r2,t,op3);
-    return r3;
+translation_A(long N){
+    long a;
+    a = N/1000000;
+
+    if(a!=0){
+        translation_B(a);
+        printf("million ");
+    }
+    a = N % 1000000;
+    if(a!=0){
+        translation_B(a);
+    }
 }
 
-float model2(float i, float j, float k, float t, int op1, int op2, int op3){
-    float r1, r2, r3;
-    r1 = cal(j,k,op2);
-    r2 = cal(i,r1,op1);
-    r3 = cal(r2,t, op3);
-    return r3;
+translation_B(long N){
+    long a;
+    a = N/1000;
+
+    if(a!=0){
+        translation_C(a);
+        printf("thousand ");
+    }
+    a = N % 1000;
+    if(a!=0){
+        translation_C(a);
+    }
 }
 
-float model3(float i, float j, float k, float t, int op1, int op2, int op3){
-    float r1,r2,r3;
-    r1 = cal(k,t,op3);
-    r2 = cal(j,r1,op2);
-    r3 = cal(i,r2,op1);
-    return r3;
+translation_C(long a){
+    long b;
+    b = a/100;
+
+    if(b!=0){
+    translation_D(b);
+    printf("hundred ");
+    }
+
+    b = a % 100;
+    if(b!=0){
+        translation_D(b);
+    }
+
 }
 
-float model4(float i, float j,float k, float t, int op1, int op2, int op3){
-    float r1,r2,r3;
-    r1 = cal(j,k,op2);
-    r2 = cal(r1,t,op3);
-    r3 = cal(i,r2,op1);
-    return r3;
-}
-
-float model5(float i, float j, float k,float t,int op1, int op2, int op3){
-    float r1,r2,r3;
-    r1 = cal(i,j,op1);
-    r2 = cal(k,t,op3);
-    r3 = cal(r1,r2,op2);
-    return r3;
-}
-
-int get24(int i, int j, int k, int t){
-    int op1, op2, op3;
-    int flag = 0;
-    for(op1 = 1; op1<=4;op1++){
-        for(op2 = 1; op2<=4; op2++){
-            for(op3 = 1; op3<=4; op3++){
-                if(model1(i,j,k,t,op1,op2,op3) == 24){
-                    printf("((%d%c%d)%c%d)%c%d=24\n",i,operation[op1],j,operation[op2],k,operation[op3],t);
-                    flag = 1;
-                }
-                if(model2(i,j,k,t,op1,op2,op3) == 24){
-                        printf("(%d%c(%d%c%d))%c%d=24\n", i,operation[op1],j,operation[op2],k,operation[op3],t);
-                        flag = 1;
-                }
-                if(model3(i,j,k,t,op1,op2,op3) == 24){
-                        printf("%d%c(%d%c(%d%c%d))=24\n",i,operation[op1],j,operation[op2],k,operation[op3],t);
-                        flag = 1;
-                }
-                if(model4(i,j,k,t,op1,op2,op3) == 24){
-                    printf("%d%c((%d%c%d)%c%d)=24\n",i,operation[op1],j,operation[op2],k,operation[op3],t);
-                    flag=1;
-                }
-                if(model5(i,j,k,t,op1,op2,op3) == 24){
-                    printf("(%d%c%d)%c(%d%c%d)=24\n",i,operation[op1],j,operation[op2],k,operation[op3],t);
-                    flag = 1;
-                }
-            }
+translation_D(long b){
+    long c;
+    if(b<=19){
+        printf("%s ", data1[b-1]);
+    }
+    else{
+        c = b/10;
+        printf("%s ", data2[c-2]);
+        c = b%10;
+        if(c!=0){
+            printf("%s ", data1[c-1]);
         }
     }
-    return flag;
 }
 int main()
 {
-    int i,j,k,t;
-    printf("Please input four integer (1-10): \n");
-    scanf("%d %d %d %d", &i,&j,&k,&t);
-
-    if(i<1||i>10 || j<1||j>10 || k<1||k>10 || t<1||t>10){
-        printf("Input illegal, please input again.");
-    }
-        if(get24(i,j,k,t));
-        else{
-            printf("Sorry, these four numbers cannot be calculated to get 24\n");
-        }
-        getche();
-
+    long N;
+    printf("Please input a number:\n");
+    scanf("%ld",&N);
+    translation(N);
+    printf(".");
+    getche();
     return 0;
 }
